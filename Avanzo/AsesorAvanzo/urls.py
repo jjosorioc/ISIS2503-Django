7d253f16fbd0861328da927600a0479d9873d94c
Pablo <p.guatibonza@uniandes.edu.co>
2022-09-30 18:58:54 -0500

    barra busqueda

--- Avanzo/AsesorAvanzo/urls.py
+++ Avanzo/AsesorAvanzo/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('asesor/', views.listaSolicitudes),
+    path('asesor/', views.listaSolicitudes, name="listaSolicitudes"),
    
 ]
--- Avanzo/AsesorAvanzo/views.py
+++ Avanzo/AsesorAvanzo/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import render
 from .logic.logic_asesorAvanzo import  get_solicitudes, getSolicitudesByEmpleado
 
 
-def listaSolicitudPorEmpleado(request, id):
-    solicitudes = getSolicitudesByEmpleado(id)
-
+def listaSolicitudPorEmpleado(request):
+    
+    
 
     return render(request, 'asesor.html', {'solicitudesFilter': solicitudes})
 
 def listaSolicitudes(request):
     solicitudes = get_solicitudes()
+    
+    id=request.GET.get('q')
+    print("id " ,id)
+    solicitudes = getSolicitudesByEmpleado(id)
     
     return render(request, 'asesor.html', {'solicitudes': solicitudes})
