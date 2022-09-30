41f75be656f1a7a96d7a7389d562bb62a0da3150
Sebastian Alberto Umaña Peinado <s.umanap@uniandes.edu.co>
2022-09-30 05:24:57 +0000

    Updated settings.py

--- Avanzo/Avanzo/settings.py
+++ Avanzo/Avanzo/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "AsesorAvanzo",
+    "Documento",
+    "EmpleadoAfiliado",
+    "EmpresaAfiliada",
+    "Solicitud"
+
 ]
 
 MIDDLEWARE = [
