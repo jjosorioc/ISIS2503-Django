131f62d87c90cbe9f30cee0937604321ca0f30cf
Pablo <p.guatibonza@uniandes.edu.co>
2022-09-30 16:34:54 -0500

    html arreglado

--- Avanzo/Avanzo/views.py
+++ Avanzo/Avanzo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'Avanzo/Avanzo/templates/index.html') #TODO: Arreglar esto
+    return render(request, 'index.html') #TODO: Arreglar esto
 
 def healthCheck(request):
     return HttpResponse("OK")
