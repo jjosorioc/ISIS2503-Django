de455c48c711375802651b9bae0c751767a86897
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-09-30 17:50:03 -0500

    Cambios para el UI

--- Avanzo/EmpleadoAfiliado/logic/logic_empleadoAfiliado.py
+++ Avanzo/EmpleadoAfiliado/logic/logic_empleadoAfiliado.py
@@ -1,7 +1,24 @@
 
-from empleado.models import Empleado
+from Avanzo.Solicitud.models import Solicitud
+from EmpleadoAfiliado.models import EmpleadoAfiliado
 
 def get_empleados():
-    queryset = Empleado.objects.all()
+    queryset = EmpleadoAfiliado.objects.all()
     return queryset
 
+
+def get_solicitudes():
+    queryset = Solicitud.objects.all()
+    return queryset
+
+
+def get_solicitudesByEmpleado(empleado):
+    queryset = Solicitud.objects.filter(empleadoAfiliado=empleado)
+
+    return queryset
+
+
+def get_empleado_by_identificacion(id: str):
+
+    query = EmpleadoAfiliado.objects.get(identificacion=id)
+    return query
--- Avanzo/EmpleadoAfiliado/views.py
+++ Avanzo/EmpleadoAfiliado/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from logic.logic_empleadoAfiliado import get_solicitudesByEmpleado, get_empleado_by_identificacion
 
-# Create your views here.
+
+
+def solicitudByEmpleado(request, empleado: str):
+    objetoEmpleado = get_empleado_by_identificacion(empleado)
+    solicitudes = get_solicitudesByEmpleado(objetoEmpleado)
+
+    return render(request, 'asesor.html', {'solicitudes': solicitudes})
+    
