00a513b655d1d158762cb2054c7ce517f3983679
Pablo <p.guatibonza@uniandes.edu.co>
2022-10-19 16:37:27 -0500

    Opción de subir documentos a las solicitudes

--- Avanzo/Solicitud/views.py
+++ Avanzo/Solicitud/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from Solicitud.logic.logic_solicitud import getSolicitudesByEmpleado
 from Solicitud.models import Solicitud
-
+from django.core.files.storage import FileSystemStorage
 from Solicitud.logic.logic_solicitud import get_solicitudes
 
 def listaSolicitudes(request):
@@ -21,7 +21,17 @@ def getSolicitudById(id):
     queryset=Solicitud.objects.get(id=id)
     return queryset
 
+
 def detailSolicitud(request, id):
     print("id: ", id)
-    solicitud=getSolicitudById(id)
+    solicitud = getSolicitudById(id)
+    if request.method=="POST":
+        uploaded_file=request.FILES['file']
+        tipo=request.POST.get('tipo')
+        fs=FileSystemStorage()
+        nombre="documentos/"+str(solicitud.id) + "_" + tipo + ".pdf"
+        name=fs.save(nombre, uploaded_file)
+    
     return render(request, 'detailSolicitud.html', {'solicitud': solicitud})
+
+
