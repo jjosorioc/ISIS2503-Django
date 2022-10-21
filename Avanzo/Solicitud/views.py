cb66aab4bf2944f72964390c437aee78a2416509
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-10-21 12:55:18 -0500

    Certificacion Bancaria y Comprobante de Pago

--- Avanzo/Solicitud/views.py
+++ Avanzo/Solicitud/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from Documento.models import CertificacionLaboral
+from Documento.models import ComprobantePago
+from Documento.models import CertificacionBancaria
+from Documento.models import Cedula
 from Solicitud.logic.logic_solicitud import getSolicitudesByEmpleado
 from Solicitud.models import Solicitud
 from django.core.files.storage import FileSystemStorage
@@ -78,17 +81,139 @@ def mapeoSegunDocumento(path: str, tipo: str, idSolicitud: int) -> bool:
 
 
 def mapearComprobantePago(doc, idSolicitud: int):
-    text = pytesseract.image_to_string(doc, lang='spa')
-    print(text)
+    text = pytesseract.image_to_string(doc, lang='eng+spa')
+    listaSegunLineas = text.split('\n')
+    atributos = {
+        'tipo': 'CP',
+        'solicitud': Solicitud.objects.get(id=idSolicitud),
+        'empresaComprobante': None, # FK de EmpresaAfiliada
+        'idComprobante': None,
+        'destinatario': None,
+        'cantidad': None,
+        'ciudad': None,
+        'fecha': None,
+        'direccion': None
+        
+    }
+
+    for linea in listaSegunLineas:
+
+        for i in ['empresa', 'compañía', 'empresa afiliada']:
+            if i in linea.lower():
+                nombreObtenido = linea.split(': ')[1].replace('  ', ' ')
+                try:
+                    objectEmpresa = EmpresaAfiliada.objects.get(nombre=nombreObtenido)
+                    atributos['empresaComprobante'] = objectEmpresa
+                except:
+                    atributos['empresaComprobante'] = None
+        for i in ['comprobante', 'número', 'numero']:
+            if i in linea.lower():
+                atributos['idComprobante'] = linea.split(': ')[1].replace('  ', ' ') 
+        for i in ['destinatario', 'beneficiario', 'cliente']:
+            if i in linea.lower():
+                atributos['destinatario'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['cantidad', 'valor', 'valor total']:
+            if i in linea.lower():
+                num = linea.split(': ')[1].strip(" $.")
+                num = num.replace('.','')
+                atributos['cantidad'] = float(num.replace(',','.'))
+        for i in ['ciudad', 'ciudad de']:
+            if i in linea.lower():
+                atributos['ciudad'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['fecha', 'fecha de']:
+            if i in linea.lower():
+                # Año-Mes-Día
+                fechaString = linea.split(': ')[1].replace('  ', ' ').replace('/', '-')
+                
+                atributos['fecha'] =  fechaString
+        for i in ['dirección', 'direccion', 'lugar']:
+            if i in linea.lower():
+                atributos['direccion'] = linea.split(': ')[1].replace('  ', ' ')
+
+    for key in atributos:
+        if atributos[key] == None:
+            return False
+
+    ComprobantePago.objects.create(
+        tipo = atributos['tipo'],
+        solicitud = atributos['solicitud'],
+        empresaComprobante = atributos['empresaComprobante'],
+        idComprobante = atributos['idComprobante'],
+        destinatario = atributos['destinatario'],
+        cantidad = atributos['cantidad'],
+        ciudad = atributos['ciudad'],
+        fecha = atributos['fecha'],
+        direccion = atributos['direccion']
+    )
+    
+                
     return True
 
 def mapearCertificacionBancaria(doc, idSolicitud: int):
-    text = pytesseract.image_to_string(doc, lang='spa')
-    print(text)
+    text = pytesseract.image_to_string(doc, lang='eng+spa')
+    listaSegunLineas = text.split('\n')
+    atributos = {
+        'tipo': 'CB',
+        'solicitud': Solicitud.objects.get(id=idSolicitud),
+        'tipoDocumento': None,
+        'numDocumento': None,
+        'medioEnvio': None,
+        'correo': None,
+        'destinatario': None,
+        'bancoCuenta': None,
+        'tipoCuenta': None,
+        'numeroCuenta': None,
+    }
+
+    for linea in listaSegunLineas:
+
+        for i in ['tipo de documento', 'tipo documento']:
+            if i in linea.lower():
+                atributos['tipoDocumento'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['número de documento', 'numero de documento']:
+            if i in linea.lower():
+                atributos['numDocumento'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['medio de envío', 'medio envio', 'envío', 'envio', "medio de envio"]:
+            if i in linea.lower():
+                atributos['medioEnvio'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['correo', 'e-mail', 'email']:
+            if i in linea.lower():
+                atributos['correo'] = linea.split(': ')[1].replace('  ', ' ').replace('(W', '@')
+        for i in ['destinatario', 'beneficiario', 'cliente']:
+            if i in linea.lower():
+                atributos['destinatario'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['banco', 'banco de']:
+            if i in linea.lower():
+                atributos['bancoCuenta'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['tipo de cuenta', 'tipo cuenta']:
+            if i in linea.lower():
+                atributos['tipoCuenta'] = linea.split(': ')[1].replace('  ', ' ')
+        for i in ['número de cuenta', 'numero de cuenta']:
+            if i in linea.lower():
+                atributos['numeroCuenta'] = linea.split(': ')[1].replace('  ', ' ')
+
+    print(atributos)
+    for key in atributos:
+        if atributos[key] == None:
+            return False
+
+    CertificacionBancaria.objects.create(
+        tipo = atributos['tipo'],
+        solicitud = atributos['solicitud'],
+        tipoDocumento = atributos['tipoDocumento'],
+        numDocumento = atributos['numDocumento'],
+        medioEnvio = atributos['medioEnvio'],
+        correo = atributos['correo'],
+        destinatario = atributos['destinatario'],
+        bancoCuenta = atributos['bancoCuenta'],
+        tipoCuenta = atributos['tipoCuenta'],
+        numeroCuenta = atributos['numeroCuenta']
+    )
+    
     return True
 
 def mapearCedula(doc, idSolicitud: int):
-    text = pytesseract.image_to_string(doc, lang='spa')
+    text = pytesseract.image_to_string(doc, lang='eng+spa')
     print(text)
     return True
 
@@ -102,7 +227,7 @@ def mapearCertificacionLaboral(doc, idSolicitud: int):
     Returns:
         _type_: _description_
     """
-    text = pytesseract.image_to_string(doc, lang='spa')
+    text = pytesseract.image_to_string(doc, lang='eng+spa')
     listaSegunLineas = text.split('\n')
     atributos = {
         'tipo': 'CL',
